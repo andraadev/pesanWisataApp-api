4529ffbd5bc451904c416e00140b33b1b2f623d0
andraadev <projeksederhanadev@gmail.com>
2024-09-10 21:13:54 +0700

    update: remove unused route

--- pesanWisata-Laravel/routes/api.php
+++ pesanWisata-Laravel/routes/api.php
@@ -4,11 +4,9 @@ use App\Http\Controllers\destinationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 Route::apiResource('/users', UserController::class);
-Route::apiResource('/destination', destinationController::class);
-Route::get('/destination', [destinationController::class, 'index']);
-Route::post('/destination/add', [destinationController::class, 'store']);
+Route::apiResource('/destinations', destinationController::class);
 Route::get('/destination/{slug}', [destinationController::class, 'show']);
