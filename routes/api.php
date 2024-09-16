e63d1284a85bab3492d6b19cfa9a6640bdc933fb
andraadev <projeksederhanadev@gmail.com>
2024-09-16 11:02:11 +0700

    Merge branch Auth to pesanWisataBackend

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,11 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\destinationController;
 use App\Http\Controllers\BookingController;
 use Illuminate\Support\Facades\Route;
+// Route Users
+Route::apiResource('/users', UserController::class);
+Route::apiResource('/destinations', destinationController::class);
+Route::get('/destination/{slug}', [destinationController::class, 'show']);
+Route::apiResource('/booking', BookingController::class);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
