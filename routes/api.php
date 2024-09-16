08e713a86e6dd701ef0768766254716d9934054b
andraadev <projeksederhanadev@gmail.com>
2024-09-16 10:30:38 +0700

    feat: add login and register

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,17 @@
 <?php
+
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\destinationController;
 use App\Http\Controllers\BookingController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-Route::apiResource('/users', UserController::class);
-Route::apiResource('/destinations', destinationController::class);
-Route::get('/destination/{slug}', [destinationController::class, 'show']);
-Route::apiResource('/booking', BookingController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('admin/users', UserController::class);
+    Route::apiResource('admin/destinations', destinationController::class);
+    Route::get('admin/destination/{slug}', [destinationController::class, 'show']);
+    Route::apiResource('admin/booking', BookingController::class);
+});
